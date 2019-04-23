a8280d01c72bb4fdf41d9f7a4e19cabda66afa8f
Bruno Falbo <bruno@visimob.com>
2019-04-23 14:18:00 -0300

    ajustes

--- frontend/static/admin/js/cadastrar-produto.js
+++ frontend/static/admin/js/cadastrar-produto.js
@@ -1,11 +1,36 @@
-$(document).ready(function()
-{
+
+
+$(document).ready(function () {
     setMasks();
+    fileEvent();
 });
 
 
-function setMasks()
-{
+function setMasks() {
     // Mascara de moeda
-    $('.money').mask('000.000.000.000.000,00', {reverse: true});
+    $('.money').mask('000.000.000.000.000,00', { reverse: true, placeholder: "R$ 0,00"});
 }
+
+// INput file event
+function fileEvent()
+{
+    $('#inputfile').change(function()
+    {
+        files = this.files;
+        for (var i = 0; i < files.length; i++) 
+        {
+            let item = $($('#data-template')[0].innerHTML);
+            $(item).find('code')[0].innerHTML = files[i].name
+            $('#fileList').append(item);
+          }
+    });
+}
+
+
+function removeFile($this)
+{
+    // Ainda nÃO REMOVE O ARQUIVO, SÓ VISUAL
+    $($this).parent().remove();
+
+}
+
